b158647fdd4f1bd7c0c95f52021d56c707482c64
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-09-18 13:43:21 -0600

    Updated styles to match new figma design

--- mobile-app/src/components/StationsListItem.js
+++ mobile-app/src/components/StationsListItem.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Pressable } from '@react-native-material/core';
 
+
 const StationsListItem = (props) => {
   station = props.item;
+  console.log(typeof(station));
+  console.log(station);
   return (
+    <View style={styles.pressableWrapper}>
     <Pressable key={station.id} style={styles.item} onPress={props.onPress} pressEffect='ripple' pressEffectColor='green'>
-      <View>
+      <View style={styles.row}>
+        <Text style={styles.label}>{station.label}</Text>
+        <View style={styles.TitleAndFields}>
         <Text style={styles.title}>{station.title}</Text>
         {station.fields.map((item) => {
           return (
@@ -15,8 +21,10 @@ const StationsListItem = (props) => {
             </View>
           );
         })}
+        </View>
       </View>
     </Pressable>
+    </View>
   );
 };
 
@@ -27,20 +35,35 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+    borderRadius: 10,
+    overflow: 'hidden',
+
   },
   title: {
     marginBottom: 10,
-    fontSize: 30,
-    alignSelf: 'center',
-    textDecorationLine: true,
-  },
-  fieldsView: {
-    alignItems: 'center',
+    fontSize: 32,
   },
   fieldsitem: {
     fontSize: 22,
     margin: 5,
-  }
+  },
+  TitleAndFields: {
+    marginLeft: 20,
+    flex: 3,
+  },
+  label: {
+    textAlign: 'start',
+    fontSize: 65,
+    fontWeight: '600',
+    flex: 1,
+    letterSpacing: 5,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
 });
 
 export default StationsListItem;
