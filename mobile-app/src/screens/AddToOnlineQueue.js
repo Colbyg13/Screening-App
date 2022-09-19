bb600175430c3f3bc73faf751773115349f984ce
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-09-19 11:50:00 -0600

    Started this page, this is not done yet.

---
+++ mobile-app/src/screens/AddToOnlineQueue.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+const AddToOnlineQueue = ({ route }) => {
+  return (
+    <View><Text style={{fontSize: 24, textAlign: 'center', marginTop: 30}}>AddToOnlineQueue</Text></View>
+  )
+}
+
+export default AddToOnlineQueue
