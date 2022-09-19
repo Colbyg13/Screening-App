957ad84de9c4ae00fdaccf494fd9b6200e8f16e1
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-09-19 11:50:34 -0600

    moved conditional station 1 element to session queue.

--- mobile-app/src/screens/QueueScreen.js
+++ mobile-app/src/screens/QueueScreen.js
@@ -1,24 +1,14 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View, Text } from 'react-native';
 import { useEffect } from 'react';
 import SessionQueue from '../components/SessionQueue';
 const QueueScreen = ({ route, navigation }) => {
   const selectedStation = route.params.selectedStation;
-  console.log('STATION 1 ye or nay?: ', selectedStation.isStationOne)
-  const [isStationOne, setIsStationOne] = useState(selectedStation.isStationOne);
+  console.log('STATION 1 ye or nay?: ', selectedStation.isStationOne);
   console.log(selectedStation);
   return (
     <>
-      {/* <Text>Queue {selectedStation.title}</Text>
-      {isStationOne && (
-        <Text>I'm Station One ADD TO QUEUE BUTTON</Text>
-        )} */}
-        <SessionQueue station={selectedStation}/>
-      
-      
+      <SessionQueue station={selectedStation} />
     </>
   );
 };
