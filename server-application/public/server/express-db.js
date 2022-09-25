eb48089883fcb56ff8aa182c03ae2f64172a3a88
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-09-25 17:06:26 -0600

    Added lastRecord collection when DB connects, checks that only 1 document is inserted.

--- server-application/public/server/express-db.js
+++ server-application/public/server/express-db.js
@@ -13,6 +13,16 @@ module.exports = {
             .then(client => {
                 _db = client.db(DB_NAME);
                 console.log("Connected to DB", DB_NAME)
+                const lastRecord = _db.collection('latestRecordID');
+                lastRecord.countDocuments()
+                .then(result => {
+                    console.log("RESULT", result, typeof(result));
+                    if(result === 0) {
+                        lastRecord.insertOne({latestID: 1})
+                    }
+                    else return; //no need to insert counter document
+                })
+                
                 // sessions = db.collection('sessions');
                 // patients = db.collection('patients');
                 // customFields = db.collection('custom_fields');
