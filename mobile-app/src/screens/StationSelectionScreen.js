d281e186362d6a189f738e8e30ee335c676e6d5b
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-10-27 16:42:41 -0600

    Update StationSelectionScreen.js

--- mobile-app/src/screens/StationSelectionScreen.js
+++ mobile-app/src/screens/StationSelectionScreen.js
@@ -2,15 +2,17 @@ import React, { useEffect } from 'react';
 import StationsList from '../components/StationsList';
 import { useSessionContext } from '../contexts/SessionContext';
 import { useIsFocused } from '@react-navigation/native';
+import { useCustomDataTypesContext } from '../contexts/CustomDataContext';
 const StationSelectionScreen = ({ route, navigation }) => {
-
+  const { customDataTypes } = useCustomDataTypesContext();
+  console.log('Custom Data Types: ', customDataTypes);
   const isFocused = useIsFocused();
-  const { selectedStation, leaveStation } = useSessionContext();
-
+  const { selectedStation, leaveStation, sessionInfo } = useSessionContext();
+  console.log('Session Info: ', sessionInfo);
   // leave the station automatically
   useEffect(() => {
     if (isFocused && selectedStation) leaveStation();
-  }, [isFocused, selectedStation])
+  }, [isFocused, selectedStation]);
 
   return (
     <>
