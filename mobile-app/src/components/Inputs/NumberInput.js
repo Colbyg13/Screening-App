0330c0c44144aeb4f21a2a0aa93944335f63d631
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-11-02 19:15:50 -0600

    string and number inputs, work in progress.

---
+++ mobile-app/src/components/Inputs/NumberInput.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import { Text, View, Keyboard, } from 'react-native'
+import { TextInput } from '@react-native-material/core';
+import { styles } from '../../style/styles';
+
+const NumberInput = (props) => {
+  
+  
+  field = props.field;
+  console.log('field in number input', field);
+  const [value, setValue] = useState(false)
+  useEffect(() => {
+    setValue(props.value)
+  }, [props.value])
+  return (
+    <View key={field.name} style={styles.row}>
+    <Text style={styles.fieldName}>{field.name}:</Text>
+    <View>
+      <TextInput
+        keyboardType='number-pad'
+        returnKeyType='done'
+        onSubmitEditing={Keyboard.dismiss}
+        onChangeText={(newText) => {
+          console.log('new text in field', field, newText);
+          props.updateForm(field, newText)
+        }}
+        style={styles.fieldInput}
+        required={field.required}
+        value={value}
+      ></TextInput>
+    </View>
+  </View>
+  )
+}
+
+export default NumberInput
