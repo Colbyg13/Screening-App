ca13413350abdd7de5e7ad4034d830130ea282e5
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2022-11-27 15:30:04 -0700

    Added button to move to offline record screen.

---
+++ mobile-app/src/components/NextButton.js
@@ -0,0 +1,36 @@
+import React,  {useState, useEffect} from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { Pressable } from '@react-native-material/core';
+
+const NextButton = (props) => {
+
+  return (
+    <View style={styles.wrapper}>
+      <Pressable style={styles.btn} onPress={props.onPress} pressEffect='ripple' pressEffectColor='#4c5e75'>
+        <Text style={styles.btnText}>Next</Text>
+      </Pressable>
+    </View>
+  )
+}
+const styles = StyleSheet.create({ 
+  wrapper:  {
+    flex: 1,
+  },
+  btn: {
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    backgroundColor: '#A3CDFF',
+    height: 50,
+    width: 125,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginRight: 20,
+  },
+  btnText: {
+    fontSize: 22,
+  } 
+})
+
+export default NextButton
