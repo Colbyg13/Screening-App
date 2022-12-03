b8c59198c3d00c684713df5ff839afebbff6a347
AndrewGiles <giles.andrew33>
2022-12-03 15:37:02 -0700

    Deleted old server folder

--- server-application/public/server/listen.js
+++
@@ -1,9 +0,0 @@
-const SERVER_PORT = 3333;
-
-module.exports = APP => {
-    APP.server.listen(SERVER_PORT, () => {
-        console.log(`listening on *:${SERVER_PORT}`);
-    });
-
-    return APP;
-}
